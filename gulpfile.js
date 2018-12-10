e04b7d1d123924c9f66632eb2f070eae4b3a2b6a
Phan An <me@phanan.net>
2018-12-10 16:38:50 +0100

    Bump gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,34 @@
-require('es6-promise').polyfill();
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var livereload  = require('gulp-livereload');
-var runSequence = require('run-sequence');
-var del         = require('del');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const prefix = require('gulp-autoprefixer')
+const livereload = require('gulp-livereload')
+const del = require('del')
 
-var local       = false;
+let local = false
 
-/**
- * Clean the target directory
- * @param  {[type]}
- * @return {[type]}     [description]
- */
-gulp.task('clean', function (cb) {
-    return del(['dist/js/**', 'dist/css/**'], cb);
-});
+const createEnvBasedTask = taskFunc => local ? taskFunc().pipe(livereload()) : taskFunc()
 
-/**
- * Generates CSS from SASS
- * @return {[type]} [description]
- */
-gulp.task('sass', ['clean'], function () {
-    var task = gulp.src('src/sass/main.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(prefix('last 2 version'))
-        .pipe(gulp.dest('dist/css'));
+gulp.task('clean', cb => del(['dist/js/**', 'dist/css/**'], cb))
 
-    if (local)
-        task = task.pipe(livereload());
+gulp.task('sass', gulp.series('clean', () => {
+    return createEnvBasedTask(() => {
+        return gulp.src('src/sass/main.scss')
+            .pipe(sass({
+                outputStyle: 'compressed'
+            }))
+            .pipe(prefix('last 2 version'))
+            .pipe(gulp.dest('dist/css'))
+    })
+}))
 
-    return task;
-});
+gulp.task('img', () => createEnvBasedTask(() => gulp.src('src/img/**/*').pipe(gulp.dest('dist/img'))))
 
-/**
- * Copy the images
- * @param  {[type]} )
- * @return {[type]}   [description]
- */
-gulp.task('img', function () {
+gulp.task('watch', () => {
+    local = true
+    livereload.listen()
 
-    var task = gulp.src('src/img/**/*')
-        .pipe(gulp.dest('dist/img'));
+    gulp.watch('src/sass/**/*.scss',  ['sass'])
+    gulp.watch('src/img/**/*', ['img'])
+})
 
-    if (local)
-        task = task.pipe(livereload());
-
-    return task;
-});
-
-/**
- * Watch files and run tasks if they change
- * @return {[type]} [description]
- */
-gulp.task('watch', function () {
-    local = true;
-    livereload.listen();
-
-    gulp.watch('src/sass/**/*.scss',  ['sass']);
-    gulp.watch('src/img/**/*', ['img']);
-});
-
-
-/**
- * The default task (called when you run `gulp`)
- */
-gulp.task('default', function(cb) {
-    runSequence('clean', ['sass', 'img'], cb);
-});
+gulp.task('default', gulp.series('clean', gulp.parallel('sass', 'img'), cb => cb()))
