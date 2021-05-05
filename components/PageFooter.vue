8f8305c4dff5712f3967eabce47f3fafb055238a
Phan An <me@phanan.net>
2021-05-05 09:33:19 +0200

    feat: add Render as a sponsor

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,16 +1,21 @@
 <template>
   <footer class="z-0">
     <p>Developed and maintained by <a rel="author" href="https://phanan.net">Phan An</a>.</p>
-    <p>Hosting and CDN proudly sponsored by</p>
+    <p>Proudly sponsored by</p>
     <ul class="sponsors flex place-content-center">
       <li>
+        <a class="render" href="https://render.com" title="Render - Cloud Hosting for Developers">
+          <img alt="Render logo" src="~/assets/img/sponsors/render.svg" height="28">
+        </a>
+      </li>
+      <li>
         <a class="exoscale" href="https://www.exoscale.ch" title="Exoscale: Swiss cloud hosting">
-          <img alt="Exoscale" src="~/assets/img/sponsors/exoscale.svg" height="32" width="auto">
+          <img alt="Exoscale logo" src="~/assets/img/sponsors/exoscale.svg" height="32" width="auto">
         </a>
       </li>
       <li>
         <a class="keycdn" href="https://www.keycdn.com/?a=11519" title="KeyCDN - Content Delivery Made Easy">
-          <img alt="KeyCDN" src="https://logos.keycdn.com/keycdn-logo-white.svg" height="32">
+          <img alt="KeyCDN logo" src="~/assets/img/sponsors/keycdn.svg" height="32">
         </a>
       </li>
     </ul>
@@ -46,6 +51,19 @@ footer {
 
     li + li {
       margin-left: 1rem;
+    }
+
+    img {
+      filter: brightness(10);
+    }
+
+    @media (max-device-width: 768px) {
+      flex-direction: column;
+
+      li + li {
+        margin-left: 0;
+        margin-top: .75rem;
+      }
     }
   }
 }
