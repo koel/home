9795d06d97a0f05f7d62b5d8f85b84f309389e46
Phan An <me@phanan.net>
2021-08-08 14:38:16 +0200

    feat: add Google Play button

--- components/MobileApp.vue
+++ components/MobileApp.vue
@@ -4,34 +4,46 @@
 
     <img
       class="showcase"
-      src="~/assets/img/showcase-mobile.png" 
-      alt="Mobile Screenshots" 
+      src="~/assets/img/showcase-mobile.png"
+      alt="Mobile Screenshots"
       width="745"
-      height="auto" 
+      height="auto"
     >
 
     <div class="intro">
       <p>
-        <a href="https://github.com/koel/player">Koel Player</a> is the official iOS and Android mobile app for 
-        <span class="font-thin">koel</span>. 
-        Connect to your <span class="font-thin">koel</span>-powered server and enjoy the music whenever you may roam, 
-        without the unfortunate <a href="https://docs.koel.dev/#mobile-support-and-limitation">limitations</a> of the 
+        <a href="https://github.com/koel/player">Koel Player</a> is the official iOS and Android mobile app for
+        <span class="font-thin">koel</span>.
+        Connect to your <span class="font-thin">koel</span>-powered server and enjoy the music whenever you may roam,
+        without the unfortunate <a href="https://docs.koel.dev/#mobile-support-and-limitation">limitations</a> of the
         mobile web version. <br>
         <a href="https://github.com/koel/player">Open-source</a>, of course.
       </p>
     </div>
 
     <p>
-      <a 
-        class="download" 
-        href="https://apps.apple.com/de/app/koel-player/id1576886982?l=en" 
+      <a
+        class="download-btn"
+        href="https://apps.apple.com/de/app/koel-player/id1576886982?l=en"
         title="Download on the App Store"
       >
-        <img 
-          src="~/assets/img/app-store.png" 
-          alt="Download on the App Store badge" 
-          width="192" 
-          height="auto"
+        <img
+          src="~/assets/img/app-store.png"
+          alt="Download on the App Store badge"
+          width="auto"
+          height="60"
+        />
+      </a>
+      <a
+        class="download-btn"
+        href="https://play.google.com/store/apps/details?id=phanan.koel.app"
+        title="Get it on Google Play"
+      >
+        <img
+          src="~/assets/img/google-play.png"
+          alt="Download on Google Play badge"
+          width="auto"
+          height="60"
         />
       </a>
     </p>
@@ -53,11 +65,22 @@ export default {
   max-width: 50rem;
 }
 
-a.download::after {
+a.download-btn::after {
   display: none;
 }
 
 img.showcase {
   max-width: calc(100vw - 8rem);
+}
+
+.download-btn + .download-btn {
+  margin-left: .5rem;
+}
+
+@media screen and (max-width: 414px) {
+  .download-btn {
+    display: block;
+    margin: 0 auto .5rem auto !important;
+  }
 }
 </style>
