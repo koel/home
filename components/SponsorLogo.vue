275d1abef7a3582750664d0a3d4e1f9037037ddd
Phan An <me@phanan.net>
2024-06-26 16:01:10 +0200

    chore: add Ray sponsor

--- components/SponsorLogo.vue
+++ components/SponsorLogo.vue
@@ -1,6 +1,11 @@
 <template>
   <a :href="sponsor.url" :title="`${sponsor.name} – ${sponsor.slogan}`">
-    <img :alt="`${sponsor.name} logo`" :src="sponsor.logo" class="opacity-70 hover:opacity-100 brightness-[200] h-[28px]" v-bind="$attrs">
+    <img
+      :alt="`${sponsor.name} logo`"
+      :src="sponsor.logo"
+      class="opacity-70 hover:opacity-100 h-[28px]"
+      v-bind="$attrs"
+    >
   </a>
 </template>
 
